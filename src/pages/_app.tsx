8e047bcce630ee40f6c7f5382c9c614d2edec9de
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-05-19 22:50:13 +0530

    setup thirdweb provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../styles/tailwind.css";
 import "../styles/globals.css";
+import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+
+const desiredChainId = ChainId.Rinkeby;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThirdwebProvider desiredChainId={desiredChainId}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
 
 export default MyApp;
