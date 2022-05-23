fa5715c00d9f9c6f7943fc89c8642604065ff4d7
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-05-23 18:15:32 +0530

    refactor: add utility to fetch allCollections or single collection by contract address

---
+++ src/utils/sanity.tsx
@@ -0,0 +1,31 @@
+import { sanityClient } from "../sanityClient";
+
+export const getCollections = async (contractAddress?: string) => {
+  let query = "";
+  if (typeof contractAddress === "string") {
+    query = `* [ _type == "marketItems" && contractAddress == "${contractAddress}"] {
+      _id , title , description , contractAddress,
+      "ownerImage": profileImage.asset->url,
+      "bannerImage": bannerImage.asset->url,
+      volumeTraded,
+      "createrName": createdBy -> userName,
+      "createrAddress": createdBy -> walletAddress
+    }`;
+  } else {
+    query = `* [ _type == "marketItems" ] {
+      _id , title , description , contractAddress,
+      "ownerImage": profileImage.asset->url,
+      "bannerImage": bannerImage.asset->url,
+      volumeTraded,
+      "createrName": createdBy -> userName,
+      "createrAddress": createdBy -> walletAddress
+    }`;
+  }
+  let collections = [];
+  try {
+    collections = await sanityClient().fetch(query);
+  } catch (error) {
+    collections = [];
+  }
+  return collections;
+};
